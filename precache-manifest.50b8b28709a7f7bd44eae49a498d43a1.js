a239852a5d9601d89d733118d35d3c0c2e7cfd7a
matt765 <mateusz.wyrebek@gmail.com>
2021-06-04 21:50:06 +0200

    Updates

---
+++ precache-manifest.50b8b28709a7f7bd44eae49a498d43a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5739f7792cb769c42311952284cf0bc2",
+    "url": "/react-daily-dashboard/index.html"
+  },
+  {
+    "revision": "39f0585c4b7b68e72de3",
+    "url": "/react-daily-dashboard/static/css/main.2947ffa1.chunk.css"
+  },
+  {
+    "revision": "b2b6f5d8f1e88b15958c",
+    "url": "/react-daily-dashboard/static/js/2.6afb12b5.chunk.js"
+  },
+  {
+    "revision": "36911d923b564768215464dead009b99",
+    "url": "/react-daily-dashboard/static/js/2.6afb12b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07c2bbd020380ba3585",
+    "url": "/react-daily-dashboard/static/js/3.6a9fd579.chunk.js"
+  },
+  {
+    "revision": "39f0585c4b7b68e72de3",
+    "url": "/react-daily-dashboard/static/js/main.f3763946.chunk.js"
+  },
+  {
+    "revision": "c5accec54e271a908bcf",
+    "url": "/react-daily-dashboard/static/js/runtime-main.941753a3.js"
+  },
+  {
+    "revision": "d8674b44e8ff384c70051a492f652aa7",
+    "url": "/react-daily-dashboard/static/media/bg.d8674b44.jpg"
+  },
+  {
+    "revision": "966746418923d1792c3dced43857ea53",
+    "url": "/react-daily-dashboard/static/media/spinner.96674641.gif"
+  }
+]);
